feb5f8829e3246fe9d0cd3d6d84858fc1dd2ff08
sergeybabann <sergeybabann@yandex.ru>
2024-12-13 14:21:58 +0300

    промежуточное сохранение работающего кода

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -7,11 +7,21 @@ const modals = () => {
 
       modal.style.display = 'block'
       document.body.style.overflow = 'hidden'
+      // document.body.classList.add('modal-open')
     })
 
     close.addEventListener('click', () => {
       modal.style.display = 'none'
       document.body.style.overflow = ''
+      // document.body.classList.remove('modal-open')
+    })
+
+    modal.addEventListener('click', (e) => {
+      if (e.target === modal) {
+        modal.style.display = 'none'
+        document.body.style.overflow = ''
+        // document.body.classList.remove('modal-open')
+      }
     })
   }
 
